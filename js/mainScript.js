3af845760a6f36da85819db1f068b9ba8e5efc30
Carina <Cococatty@users.noreply.github.com>
2018-06-28 14:00:38 +1200

    trail with js var

---
+++ js/mainScript.js
@@ -0,0 +1,6 @@
+var h1Crown = document.querySelector('h1');
+crownH1h1Crown.textContent = 'There your Crown!';
+
+function showYeah() {
+   document.getElementById("txtYeah").innerHTML = "Yeah Yeah Yeah! I saw you want to have a Party Time!";
+}
