bf2b2514cb7ec116416e2582e694a8bc3144a6ac
Carina <Cococatty@users.noreply.github.com>
2018-06-28 14:12:13 +1200

    trial

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,5 +1,5 @@
-var h1Crown = document.querySelector('h1');
-crownH1h1Crown.textContent = 'There your Crown!';
+//var h1Crown = document.querySelector('h1');
+//crownH1h1Crown.textContent = 'There your Crown!';
 
 function showYeah() {
    document.getElementById("txtYeah").innerHTML = "Yeah Yeah Yeah! I saw you want to have a Party Time!";
