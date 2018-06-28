06d56f7352f0d9a5cf9c7d8565bb1194f372bed0
Carina <Cococatty@users.noreply.github.com>
2018-06-28 14:43:41 +1200

    wrong place

--- js/mainScript.js
+++
@@ -1,6 +0,0 @@
-var h1Crown = document.querySelector('h1');
-h1Crown.textContent = 'There your Crown!';
-
-function showYeah() {
-   document.getElementById("txtYeah").innerHTML = "Yeah Yeah Yeah! I saw you want to have a Party Time!";
-}
