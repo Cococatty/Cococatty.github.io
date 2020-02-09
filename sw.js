cdd255a9f3b28efcbfc61e035eea72761a4688cc
Cococatty <carinauc@outlook.com>
2020-02-10 01:24:07 +1300

    Updates

--- sw.js
+++ sw.js
@@ -30,7 +30,7 @@ self.__precacheManifest = [
     "url": "webpack-runtime-1eea97820dbd42c7387c.js"
   },
   {
-    "url": "commons-9974fe9d94d410392b87.js"
+    "url": "commons-4249cc137c8fadb5ef51.js"
   },
   {
     "url": "app-5e276e7211690767cd8e.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b5cb55c51d9d6a32f8c2bc077496e27e"
+    "revision": "29071ad9cb040caf8729009b2de552c1"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
