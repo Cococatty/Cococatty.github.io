1939ceaff3487025151410f9da2a5ee486312f3a
Cocatty <carinauc@outlook.com>
2026-04-26 15:44:21 +1200

    update git

--- src/constants/link-presets.ts
+++ src/constants/link-presets.ts
@@ -46,18 +46,18 @@ export const LinkPresets: { [key in LinkPreset]: NavbarLink } = {
         icon: "material-symbols:photo-library",
         description: i18n(I18nKey.albumsSubtitle),
     },
-    [LinkPreset.Anime]: {
-        name: i18n(I18nKey.anime),
-        url: "/anime/",
-        icon: "material-symbols:movie",
-        description: "A list of anime I have watched.",
-    },
-    [LinkPreset.Friends]: {
-        name: i18n(I18nKey.friends),
-        url: "/friends/",
-        icon: "material-symbols:group",
-        description: "A curated list of friend sites.",
-    },
+    // [LinkPreset.Anime]: {
+    //     name: i18n(I18nKey.anime),
+    //     url: "/anime/",
+    //     icon: "material-symbols:movie",
+    //     description: "A list of anime I have watched.",
+    // },
+    // [LinkPreset.Friends]: {
+    //     name: i18n(I18nKey.friends),
+    //     url: "/friends/",
+    //     icon: "material-symbols:group",
+    //     description: "A curated list of friend sites.",
+    // },
     [LinkPreset.About]: {
         name: i18n(I18nKey.about),
         url: "/about/",
